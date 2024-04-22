6c80788274d0154f42d8b688b1144c2f79e77818
Basar baok. Ökke <baok@weller.lan>
2024-04-22 09:50:10 +0200

    Added Task Factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\Task;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -13,11 +14,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        Task::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'User 1',
+            'email' => 'test@test.com',
+        ]);
+        
+        User::factory()->create([
+            'name' => 'User 2',
+            'email' => 'test2@test.com',
         ]);
     }
 }
